d0fb629533f31b1c6cc5702ee38dd766489562e4
leoschopen <1428088096@qq.com>
2022-01-27 12:25:19 +0800

    '初始化'

---
+++ task_platform/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, re_path
+from django.conf import settings
+from django.views.static import serve
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('create-task/', views.create_task, name='create-task'),
+    path('detail/tk<int:task_id>/', views.detail, name='detail'),
+    path('upload/', views.sceneImgUpload, name='uploadimg'),
+    path('upload/&responseType=json', views.sceneImgUpload),
+    path('profile/', views.profile, name='profile'),
+    path('chatroom/<str:room_id>', views.chatroom, name='chatroom'),
+    path('guide/', views.guide, name='guide'),
+    path('about/', views.about, name='about'),
+    path('image/<str:img_id>', views.image_sight, name='image'),
+    path('task-search/', views.search, name='search'),
+]
